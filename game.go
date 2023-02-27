ac10063391a3cc502fc84f5c16da6888791c3557
Dr. Brantley <dr.brantley@redhat.com>
2023-02-27 14:41:09 -0500

    Update game tiles. Add snake head. Begin work on game states.

--- game.go
+++ game.go
@@ -2,26 +2,58 @@ package main
 
 import (
 	_ "image/png"
+	"image/color"
 	"log"
 	"strings"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text"
 )
 
 const (
 	screenWidth  = 640
 	screenHeight = 640
+	dpi = 72
+	baseFontSize = 24
 	gameTitle = "Go Snake"
+	startGameText = "Use arrow keys to guide Snake.\n    Press Enter to start."
 )
 
-var tilesImage *ebiten.Image
+var (
+	tilesImage *ebiten.Image
+	snakeHead *ebiten.Image
+	baseFont font.Face
+	gameActive bool
+)
 
 func init() {
 	// Load background image
 	var err error
 	tilesImage, _, err = ebitenutil.NewImageFromFile("images/tiles.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Load snake head
+	snakeHead, _, err = ebitenutil.NewImageFromFile("images/snake-head.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Load basic font
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	baseFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    baseFontSize,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,10 +72,21 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// Draw background
 	screen.DrawImage(tilesImage, nil)
 
+	// Draw snake head
+	screen.DrawImage(snakeHead, nil)
+
+	// Draw start game text
+	if ( gameActive == false ) {
+		text.Draw(screen, startGameText, baseFont, (screenWidth/3)-50, (screenHeight/3)+90, color.White)
+	}
+
 	// Get keys pressed
 	keyStrs := []string{}
 	for _, k := range g.keys {
 		keyStrs = append(keyStrs, k.String())
+		if ( k.String() == "Enter" ){
+			gameActive = true
+		}
 	}
 
 	// Debug key presses
